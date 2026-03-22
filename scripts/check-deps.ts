6b5b21ebb701d2736424ab2911c588d9439b3834
Alex Breen <github@abreen.email>
2026-03-22 09:25:45 -0400

    Check for bun and python3 and warn developers

---
+++ scripts/check-deps.ts
@@ -0,0 +1,7 @@
+if (!Bun.which('bun')) {
+  console.error('Warning: Bun is required to develop this project.');
+}
+
+if (!Bun.which('python3')) {
+  console.error('Warning: python3 is required to run functional tests.');
+}
