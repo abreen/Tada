263c9c0f84ce13c9f636f507011e8362e361000e
Alex Breen <github@abreen.email>
2026-03-30 20:48:40 -0400

    Add recursive example to trace

--- init/content/labs/01/TraceDemo.java
+++ init/content/labs/01/TraceDemo.java
@@ -1,10 +1,18 @@
 public class TraceDemo {
+    private static int sum(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        return n + sum(n - 1);
+    }
     public static void main(String[] args) {
         int[] arr = new int[5];
         for (int i = 0; i < arr.length; i++) {
             int n = (int)Math.pow(2, i);
             arr[i] = n;
         }
+
+        int total = sum(4);
 
         String str = "hello";
         for (int i = 0; i < str.length(); i++) {
