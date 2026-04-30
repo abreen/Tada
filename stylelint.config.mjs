895b87ca1e4faabb5ca4f702a15611bcaa3b3912
Alex Breen <github@abreen.email>
2026-04-29 21:45:10 -0400

    Skip Bun-incompatible linting

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -2,6 +2,14 @@ export default {
   extends: ['stylelint-config-standard-scss', 'stylelint-config-recess-order'],
   customSyntax: 'postcss-scss',
   rules: {
+    /*
+     * These stylelint-scss rules are not currently compatible with Bun due to
+     * CommonJS default import issues in postcss-media-query-parser.
+     * This should not be an issue since we have Prettier for code formatting.
+     */
+    'scss/operator-no-newline-after': null,
+    'scss/operator-no-newline-before': null,
+    'scss/operator-no-unspaced': null,
     'declaration-block-no-duplicate-properties': [
       true,
       {
